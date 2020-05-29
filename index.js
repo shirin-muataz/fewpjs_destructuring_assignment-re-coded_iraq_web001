231574081afebb88e843f530d49951e6375ada42
Shirin Muataz <shirinmuataz@gmail.com>
2020-05-29 01:02:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const [ , , , , , indg,]=colors;
 const name= muppet.name;
 const color=muppet.color;
 const song=muppet.song;
-  job: 'Host of The Muppet Show',
+const job=muppet.job;
   partner: 'Miss Piggy'
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
